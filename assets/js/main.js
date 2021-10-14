7b08b9ffb13111cc0cd958bb4e437cbbc9700808
JairoVera01 <fj.verachamorro@gmail.com>
2021-10-13 23:09:51 -0500

    MiViajePersonal-Avance
    Avance de mis trabajos y linea de carrea en trabajos posteriores y actuales 13/10/21 11:09pm

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,8 +33,26 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== ACORDEON DE HABILIDADES ====================*/
+//Codigo para abrir y cerrar el acordeon de habilidades
 
+const skillsContet = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i< skillsContet.length; i++){
+        skillsContet[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 

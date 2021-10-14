697846291f2f51728afc3133a52b4c8b086aa7be
JairoVera01 <fj.verachamorro@gmail.com>
2021-10-14 15:06:04 -0500

    Seccion servicio
    Avance hasta seccion servicios

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,25 @@ skillsHeader.forEach((el) => {
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+//Cambiar entre Educacion y trabajos
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.database.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
